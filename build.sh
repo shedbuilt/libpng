fdec5717295fc3c8b09f639425686e67988cc328
Auston Stewart <auston@shedbuilt.net>
2018-04-15 12:15:51 -1000

    Updating packaging for blank

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-patch -Np1 -i "${SHED_PATCHDIR}/libpng-1.6.34-apng.patch" && \
+patch -Np1 -i "${SHED_PKG_PATCH_DIR}/libpng-1.6.34-apng.patch" && \
 LIBS=-lpthread ./configure --prefix=/usr \
                            --disable-static && \
-make -j $SHED_NUMJOBS && \
-make DESTDIR="$SHED_FAKEROOT" install || exit 1
+make -j $SHED_NUM_JOBS && \
+make DESTDIR="$SHED_FAKE_ROOT" install || exit 1
 install -dm755 /usr/share/doc/libpng-1.6.34
 install -m644 README /usr/share/doc/libpng-1.6.34
 install -m644 libpng-manual.txt /usr/share/doc/libpng-1.6.34
